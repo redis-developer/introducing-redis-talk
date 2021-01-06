b7419f03e8659e040223bfd551b7eed74bd81295
Simon Prickett <simon@crudworks.org>
2021-01-06 02:37:30 +0000

    Slows rate of job creation.

---
+++ queuing/job_producer.py
@@ -0,0 +1,32 @@
+import json
+import random
+import redis
+import time
+
+LIST_KEY = "jobs"
+
+JOBS = [
+    "Cleaning",
+    "Room Service",
+    "Taxi",
+    "Extra Towels",
+    "Extra Pillows"
+]
+
+r = redis.Redis(decode_responses = True)
+
+r.delete(LIST_KEY)
+
+while True:
+    job = {
+        "room": random.randint(100, 500),
+        "job": random.choice(JOBS)
+    }
+
+    payload = json.dumps(job)
+    list_length = r.lpush(LIST_KEY, payload)
+
+    print("Added:")
+    print(payload)
+    print(f"Queue length is {list_length}")
+    time.sleep(random.randint(1, 10))
