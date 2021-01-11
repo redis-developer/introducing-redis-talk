dc204b5c343538f8e2d125fbe4de6602ba831571
Simon Prickett <simon@redislabs.com>
2021-01-11 23:09:04 +0000

    Removed spurious comments.

--- full_text_search_and_secondary_indexing/query_index.py
+++ full_text_search_and_secondary_indexing/query_index.py
@@ -3,19 +3,15 @@ from redisearch import Client
 client = Client("adoptables")
 
 def find_by_name():
-    # FT.SEARCH adoptables Luna
     return client.search("Luna")
 
 def find_dogs_in_age_range():
-    # FT.SEARCH adoptables "@species:{dog} @age:[9 15]"
     return client.search("@species:{dog} @age:[9 15]")
 
 def find_cats_good_with_children():
-    # FT.SEARCH adoptables "@species:{cat} @children:{y} @description:(-anxious -nervous)"
     return client.search("@species:{cat} @children:{y} @description:(-anxious -nervous)")
 
 def find_male_dogs_within_radius():
-    # FT.SEARCH adoptables "@species:{dog} @sex:{m} @location:[-2.2297829,53.0220219 50 mi]"
     return client.search("@species:{dog} @sex:{m} @location:[-2.2297829,53.0220219 50 mi]")
 
 matching_pets = find_cats_good_with_children()
