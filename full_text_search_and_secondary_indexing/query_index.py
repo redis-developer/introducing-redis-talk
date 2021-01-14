703cc0d0e4d4f99ca511d001014c9e6e16f8f074
Simon Prickett <simon@redislabs.com>
2021-01-14 15:13:04 +0000

    Updated which function runs by default.

--- full_text_search_and_secondary_indexing/query_index.py
+++ full_text_search_and_secondary_indexing/query_index.py
@@ -14,7 +14,7 @@ def find_cats_good_with_children():
 def find_male_dogs_within_radius():
     return client.search("@species:{dog} @sex:{m} @location:[-2.2297829,53.0220219 50 mi]")
 
-matching_pets = find_cats_good_with_children()
+matching_pets = find_by_name()
 print(f"{matching_pets.total} matches.")
 
 for matching_pet in matching_pets.docs:
