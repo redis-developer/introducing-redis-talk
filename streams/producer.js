ff39061f771e8046c278f92f9429b29f8af641d8
Simon Prickett <simon@redislabs.com>
2021-09-20 21:51:22 +0100

    Adds some boilerplate stream code.

---
+++ streams/producer.js
@@ -0,0 +1,13 @@
+const Redis = require('ioredis');
+
+const redisClient = new Redis({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD
+});
+
+const streamProducer = async () => {
+
+};
+
+streamProducer();
