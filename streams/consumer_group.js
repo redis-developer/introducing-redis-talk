db4f8ffb418ba7941f0ea8f2fc96462bdb12bedb
Simon Prickett <simon@redislabs.com>
2021-09-27 14:11:07 +0100

    Implemented consumer group example.

--- streams/consumer_group.js
+++ streams/consumer_group.js
@@ -1,6 +1,10 @@
 const redis = require('./redis');
+const utils = require('./utils');
 
 const CONSUMER_GROUP_NAME = "temphumidity_consumers";
+const BLOCK_TIME = 5000;
+const MIN_WORK_DURATION = 1000;
+const MAX_WORK_DURATION = 5000;
 
 const streamConsumerInGroup = async () => {
   if (process.argv.length !== 4) {
@@ -22,8 +26,29 @@ const streamConsumerInGroup = async () => {
 
   console.log(`Consumer group ${CONSUMER_GROUP_NAME} ${consumerGroupStatus}.`);
 
-  console.log(await redis.client.ping());
-  redis.client.quit();
+  while (true) {
+    console.log('Reading stream...');
+    const response = await redis.client.xreadgroup('GROUP', CONSUMER_GROUP_NAME, consumerName, 'COUNT', 1, 'BLOCK', BLOCK_TIME, 'STREAMS', redis.STREAM_KEY_NAME, '>');
+    
+    if (response) {
+      // Unpack some things from the array of arrays response.
+      const entry = response[0][1][0];
+      const entryId = entry[0];
+      const entryPayload = entry[1];
+      
+      console.log(`Received entry ${entryId}:`);
+      console.log(entryPayload);
+
+      // Simulate some work
+      await utils.sleep(utils.randomInRange(MIN_WORK_DURATION, MAX_WORK_DURATION, true));
+
+      // Positively acknowledge this message has been processed.
+      await redis.client.xack(redis.STREAM_KEY_NAME, CONSUMER_GROUP_NAME, entryId);
+      console.log(`Acknowledged processing of entry ${entryId}.`);
+    } else {
+      console.log('No new entries yet.');
+    }
+  }
 };
 
 streamConsumerInGroup();
