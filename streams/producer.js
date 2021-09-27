906c213e949bd5e19d5ab8c580dc353c4b3cb37f
Simon Prickett <simon@redislabs.com>
2021-09-27 13:18:30 +0100

    Refactored to use utils.js.

--- streams/producer.js
+++ streams/producer.js
@@ -1,6 +1,6 @@
 const redis = require('./redis');
+const utils = require('./utils');
 
-const KEY_NAME = 'ingest:temphumidity';
 const MIN_LOCATION_ID = 1;
 const MAX_LOCATION_ID = 100;
 const MIN_TEMP_F = 14;
@@ -10,28 +10,17 @@ const MAX_HUMIDITY = 99;
 const MIN_GAP = 1000;
 const MAX_GAP = 3000;
 
-const randomInRange = (min, max, wholeNumber) => {
-  const num = Math.random() * (max - min) + min;
-  return wholeNumber ? Math.floor(num) : Math.round(num * 10) / 10;
-};
-
-const sleep = (howLong) => { 
-  return new Promise((resolve) => {
-    setTimeout(resolve, howLong)
-  })
-};
-
 const streamProducer = async () => {
   console.log('Starting producer...');
 
   while (true) {
-    const locationId = randomInRange(MIN_LOCATION_ID, MAX_LOCATION_ID, true);
-    const temperature = randomInRange(MIN_TEMP_F, MAX_TEMP_F);
-    const humidity = randomInRange(MIN_HUMIDITY, MAX_HUMIDITY);
+    const locationId = utils.randomInRange(MIN_LOCATION_ID, MAX_LOCATION_ID, true);
+    const temperature = utils.randomInRange(MIN_TEMP_F, MAX_TEMP_F);
+    const humidity = utils.randomInRange(MIN_HUMIDITY, MAX_HUMIDITY);
     console.log(`Adding reading for location: ${locationId}, temperature: ${temperature}, humidity: ${humidity}`);
     const entryId = await redis.client.xadd(redis.STREAM_KEY_NAME, '*', 'location', locationId, 'temp', temperature, 'humidity', humidity);
     console.log(`Added as ${entryId}`);
-    await sleep(randomInRange(MIN_GAP, MAX_GAP, true));
+    await utils.sleep(utils.randomInRange(MIN_GAP, MAX_GAP, true));
   }
 };
 
