5141f2f9b01995bbdc1dcda57522f592a3ba33d0
Simon Prickett <simon@redislabs.com>
2021-09-27 11:46:40 +0100

    Completed producer code.

--- streams/producer.js
+++ streams/producer.js
@@ -1,9 +1,38 @@
 const redis = require('./redis');
 
+const KEY_NAME = 'ingest:temphumidity';
+const MIN_LOCATION_ID = 1;
+const MAX_LOCATION_ID = 100;
+const MIN_TEMP_F = 14;
+const MAX_TEMP_F = 86;
+const MIN_HUMIDITY = 25;
+const MAX_HUMIDITY = 99;
+const MIN_GAP = 1000;
+const MAX_GAP = 3000;
+
+const randomInRange = (min, max, wholeNumber) => {
+  const num = Math.random() * (max - min) + min;
+  return wholeNumber ? Math.floor(num) : Math.round(num * 10) / 10;
+};
+
+const sleep = (howLong) => { 
+  return new Promise((resolve) => {
+    setTimeout(resolve, howLong)
+  })
+};
+
 const streamProducer = async () => {
-  console.log('TODO PRODUCER');
-  console.log(await redis.client.ping());
-  redis.client.quit();
+  console.log('Starting producer...');
+
+  while (true) {
+    const locationId = randomInRange(MIN_LOCATION_ID, MAX_LOCATION_ID, true);
+    const temperature = randomInRange(MIN_TEMP_F, MAX_TEMP_F);
+    const humidity = randomInRange(MIN_HUMIDITY, MAX_HUMIDITY);
+    console.log(`Adding reading for location: ${locationId}, temperature: ${temperature}, humidity: ${humidity}`);
+    const entryId = await redis.client.xadd(redis.STREAM_KEY_NAME, '*', 'location', locationId, 'temp', temperature, 'humidity', humidity);
+    console.log(`Added as ${entryId}`);
+    await sleep(randomInRange(MIN_GAP, MAX_GAP, true));
+  }
 };
 
 streamProducer();
